baeed3fcadee1908a1a51d18ae5c663f9cba429d
ezequielceballos951 <ezequielceballos951@gmail.com>
2024-05-08 12:54:57 -0300

    actualizado

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+# Solicitar al usuario que ingrese tres números
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+num3 = float(input("Ingrese el tercer número: "))
+
+# Calcular el promedio de los números
+promedio = (num1 + num2 + num3) / 3
+
+# Mostrar el promedio
+print("El promedio de los números ingresados es:", promedio)
